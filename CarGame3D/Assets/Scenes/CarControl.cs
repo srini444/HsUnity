b84dbbf1c1582d99181f92b4d9416d98da22f72b
srini444 <srinivaschari.y@gmail.com>
2018-08-19 19:56:41 +0530

    Add  car script

---
+++ CarGame3D/Assets/Scenes/CarControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarControl : MonoBehaviour {
+
+    public float motorGucu;
+    public WheelCollider onSol;
+    public WheelCollider onSag;
+    public WheelCollider arkaSol;
+    public WheelCollider arkaSag;
+    public float donmeGucu;
+    public float frenGucu;
+
+
+
+    void Start ()
+    {
+		
+	}
+	
+	
+	void Update ()
+    {
+        float v = Input.GetAxis("Vertical") * motorGucu;
+        float h = Input.GetAxis("Horizontal") * donmeGucu;
+
+        arkaSol.motorTorque = v;
+        arkaSag.motorTorque = v;
+
+        onSol.steerAngle = h;
+        onSag.steerAngle = h;
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            arkaSol.brakeTorque = frenGucu;
+            arkaSag.brakeTorque = frenGucu;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            arkaSol.brakeTorque = 0;
+            arkaSag.brakeTorque = 0;
+
+        }
+
+    }
+}
