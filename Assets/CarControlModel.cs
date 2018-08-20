19e52fbc061966925d5fd0c8bf459addf63cad4a
srini444 <srinivaschari.y@gmail.com>
2018-08-20 17:54:06 +0530

    Add Car Script

--- Assets/CarControlModel.cs
+++ Assets/CarControlModel.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarControl : MonoBehaviour {
+public class CarControlModel : MonoBehaviour {
 
     public float motorGucu;
     public WheelCollider onSol;
