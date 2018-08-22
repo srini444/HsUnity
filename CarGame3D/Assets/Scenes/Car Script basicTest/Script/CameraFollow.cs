95b4422a218811c45e0ee42ad1cf7fed271d0892
srini444 <srinivaschari.y@gmail.com>
2018-08-22 19:06:20 +0530

    Clear Project

--- CarGame3D/Assets/Scenes/Car Script basicTest/Script/CameraFollow.cs
+++ CarGame3D/Assets/Scenes/Car Script basicTest/Script/CameraFollow.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-public class CameraFollowCar : MonoBehaviour {
+public class CameraFollow : MonoBehaviour {
 
     public Transform target;
     public float smoothSpeed = 0.125f;
