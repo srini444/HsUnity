950b881a94efea15d586630fa2deb04099bea3b3
srini444 <srinivaschari.y@gmail.com>
2018-08-22 18:50:43 +0530

    Clean Project

--- CarGame3D/Assets/NCE/Scripts/Server_Scripts/Traffic/Traffic_Node.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-
-public class Traffic_Node : MonoBehaviour {
-
-	public List<Transform> NextNodes = new List<Transform>();
-	public bool Stop = false;
-}
