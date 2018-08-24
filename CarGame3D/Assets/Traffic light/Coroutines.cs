711d37b4a8e074d13f3872330c3e669b5177854d
srini444 <srinivaschari.y@gmail.com>
2018-08-24 16:01:18 +0530

    Add new projects

---
+++ CarGame3D/Assets/Traffic light/Coroutines.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coroutines : MonoBehaviour {
+
+    public float speed = 1f;
+    // Use this for initialization
+    void Start ()
+    {
+        StartCoroutine("cotest");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = new Vector3(transform.position.x + Time.deltaTime * speed, transform.position.y, transform.position.z);
+        }
+
+       
+
+    }
+
+    IEnumerator cotest()
+    {
+     
+       
+        Debug.Log("Stated");
+        yield return new WaitForSeconds(60f);
+       
+        Debug.Log("End");
+    }
+
+}
---
+++ CarGame3D/Assets/Traffic light/TrafficLightOneByOne.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficLightOneByOne : MonoBehaviour {
+
+    // Red Light
+    public Renderer redLight;
+
+    public Material redLightON;
+    public Material redLightOFF;
+
+    // Yellow Light
+    public Renderer yellowLight;
+
+    public Material yellowLightON;
+    public Material yellowLightOFF;
+
+
+    bool level1, level2, level3;
+    float timer = 0;
+	void Start ()
+    {
+        
+	}
+	
+	
+	void Update ()
+    {
+
+        timer += Time.deltaTime;
+        if (timer > 5)
+        {
+            redLightOFF;
+        }
+
+	}
+}
