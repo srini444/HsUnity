6bee27b6c9d8733de1d545c0e3588aa58631f8bd
srini444 <srinivaschari.y@gmail.com>
2018-08-25 00:06:28 +0530

    Final done change camera and trafic light done

--- CarGame3D/Assets/CarRace 3D Srinivas/Car Script basicTest/Script/CameraFollow.cs
+++ CarGame3D/Assets/CarRace 3D Srinivas/Car Script basicTest/Script/CameraFollow.cs
@@ -5,19 +5,19 @@ public class CameraFollow : MonoBehaviour {
     public Transform target;
     public float smoothSpeed = 0.125f;
     public Vector3 offsetRot;
-   public Vector3 offset;
+    public Vector3 offset;
 
     void Start()
     {
         //offset = target.transform.position - transform.position;
     }
-    void FixedUpdate()
+    void Update()
     {
 
-        Vector3 desiredPosition = target.position + offset;
+    /*    Vector3 desiredPosition = target.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
 
-        transform.position = smoothedPosition;
+        transform.position = smoothedPosition;*/
         transform.LookAt(target);
 
     }
