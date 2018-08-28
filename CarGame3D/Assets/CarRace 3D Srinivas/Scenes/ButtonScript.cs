93782ba8b39025eb2df46479319f6eb6b507dd68
srini444 <srinivaschari.y@gmail.com>
2018-08-28 17:57:51 +0530

    MainMenu

---
+++ CarGame3D/Assets/CarRace 3D Srinivas/Scenes/ButtonScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void TrackSelect()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Track01()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Track02()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+   
+
+}
