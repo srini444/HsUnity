ffdbd6f1e04f795908e9668c5ab9c6b7802616a6
srini444 <srinivaschari.y@gmail.com>
2018-09-07 17:57:57 +0530

    AR Demo project

---
+++ ARCards/Assets/Scripts/RotateScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateScript : MonoBehaviour {
+		Vector3 movement;
+	// Use this for initialization
+	void Start () 
+	{
+		movement = new Vector3 (150, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Rotate (movement* Time.deltaTime);
+
+	}
+
+}
