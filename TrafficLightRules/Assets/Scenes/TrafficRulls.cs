ef149a512e17f5405d79e508bb6bc8f7ba2aa2f3
srini444 <srinivaschari.y@gmail.com>
2018-09-08 17:47:28 +0530

    Add New Project

---
+++ TrafficLightRules/Assets/Scenes/TrafficRulls.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficRulls : MonoBehaviour 
+{
+
+	public GameObject Red;
+	public GameObject Yellow;
+	public GameObject Green;
+
+	public Renderer redLight;
+	public Renderer yellowLight;
+	public Renderer greenLight;
+
+
+	public Color brightRed;
+	public Color dullRed;
+
+	public Color brightYellow;
+	public Color dullYellow;
+
+	public Color brightGreen;
+	public Color dullGreen;
+
+	bool level1, level2, level3;
+
+	float timer = 0;
+	void Start()
+	{
+		/*Red = transform.Find ("Red").gameObject;
+		Yellow = transform.Find ("Yellow").gameObject;
+		Green = transform.Find ("Green").gameObject;
+
+		Red = Red.GetComponent<Renderer> ();
+		Yellow = Yellow.GetComponent<Renderer>();
+		Green = Green.GetComponent<Renderer>();
+		*/
+	}
+
+	void Update()
+	{
+
+
+
+
+
+	}
+
+}
---
+++ TrafficLightRules/Assets/Scripts/TrafficLightEast.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class TrafficLightEast : MonoBehaviour {
+
+    
+    public GameObject RedLight;
+    public GameObject YellowLight;
+    public GameObject GreenLight;
+    
+    
+    public Renderer redLight;
+    public Renderer yellowLight;
+    public Renderer greenLight;
+    
+
+    public Color brightYellow;
+    public Color dullYellow;
+
+    public Color brightRed;
+    public Color dullRed;
+
+    public Color brightGreen;
+    public Color dullGreen;
+
+    bool level1, level2, level3;
+ 
+    float timer = 0;
+	void Start ()
+    {
+        YellowLight = transform.Find("Yellow").gameObject;
+        RedLight = transform.Find("Red").gameObject;
+        GreenLight = transform.Find("Green").gameObject;
+
+       
+
+        yellowLight = YellowLight.GetComponent<Renderer>();
+        redLight = RedLight.GetComponent<Renderer>();
+        greenLight = GreenLight.GetComponent<Renderer>();
+
+
+		 redLight.material.color = dullRed;
+		 yellowLight.material.color = dullYellow;
+		 greenLight.material.color = brightGreen;
+     
+    }
+	
+	
+	void Update ()
+    {
+
+        timer += Time.deltaTime;
+        if (timer > 5 && timer<10 && level1 == false)
+        {
+			
+            greenLight.material.color = dullGreen;
+			redLight.material.color = brightRed;
+
+            level1 = true;
+        }
+
+        
+        if (timer > 10 && timer<15 && level2 == false)
+        {
+			redLight.material.color = dullRed;
+			yellowLight.material.color = brightYellow;
+
+            
+            level2 = true;
+           
+
+        }
+
+        
+        if (timer >15)
+        {
+            
+			yellowLight.material.color = dullYellow;
+			greenLight.material.color = brightGreen;
+            
+            level1 = false;
+            level2 = false;
+            timer = 0;
+
+        }
+
+
+    }
+}
