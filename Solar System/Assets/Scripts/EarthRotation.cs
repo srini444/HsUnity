59bef9640d5d765c71b45ba2aef793b956aeb2f6
Srinivas <srinivaschari.y@gmail.com>
2018-09-15 18:08:48 +0530

    Sololar System

---
+++ Solar System/Assets/Scripts/EarthRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthRotation : MonoBehaviour {
+
+	float earthSpeed = 0.5f;
+
+	void Start () 
+	{
+		
+	}
+
+
+	void Update () 
+	{
+		transform.Rotate (0, earthSpeed+Time.deltaTime, 0, Space.World);
+
+	}
+
+}
