570045c697696892f11a4a8095b1c7f21a806d70
Srinivas <srinivaschari.y@gmail.com>
2018-09-21 16:04:06 +0530

    Ray

---
+++ ShootingGame/Assets/RaycastGun.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastGun : MonoBehaviour {
+
+	public GameObject enemy;
+	void Start () 
+	{
+//
+//        for (int t = 0; t < 6; t++)
+//        {
+//            for (int r = 0; r < 6; r++)
+//            {
+//                GameObject temp = (GameObject)Instantiate(pawn, new Vector3(-2 + 1.5f * r, .5f , 4 - 1.5f * t),
+//                                                               
+//                    Quaternion.identity);
+//            }
+//        }
+//
+	}
+	
+
+	void Update () {
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit vHit;
+        
+        if (Physics.Raycast(ray, out vHit,100))
+        {
+            Debug.DrawRay(Camera.main.transform.position, (vHit.point - Camera.main.transform.position)*50,Color.black);
+
+			if (vHit.transform.tag == "enemy")
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    if (vHit.transform.GetComponent<Renderer>().material.color != Color.black)
+                    { 
+                    vHit.transform.GetComponent<Renderer>().material.color = Color.black;
+                    }
+                     else
+                    {
+                    vHit.transform.GetComponent<Renderer>().material.color = Color.white;
+                	}
+                }
+                
+            }
+        }
+	}
+}
