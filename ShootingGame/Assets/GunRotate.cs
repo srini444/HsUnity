86acb18a35cebfa742bbff7fdceb87cf0b4637a7
Srinivas <srinivaschari.y@gmail.com>
2018-09-21 15:17:32 +0530

    GunShot

---
+++ ShootingGame/Assets/GunRotate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunRotate : MonoBehaviour {
+
+	public float GunRouondSpeed;
+	public GameObject Gun;
+
+
+	void Start () 
+	{
+		
+	}
+	
+
+	void Update () 
+	{
+		transform.RotateAround (Gun.transform.position, Vector3.up, GunRouondSpeed * Time.deltaTime);
+		//GunAround ();
+	}
+
+	public void GunAround ()
+	{
+		
+	}
+
+}
