c0c90ff0c2e5bc003b10ce4af58da18d1704ab9c
Srinivas <srinivaschari.y@gmail.com>
2018-09-26 15:26:58 +0530

    Add Shoting

---
+++ ShootingGame/Assets/RotateObjects.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObjects : MonoBehaviour {
+
+	public float rotateSpeed;
+	void Start () 
+	{
+		
+	}
+	
+
+	void Update () 
+	{
+		transform.Rotate (0, rotateSpeed*Time.deltaTime, 0);
+	}
+}
