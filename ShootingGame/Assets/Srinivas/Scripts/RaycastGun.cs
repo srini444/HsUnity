615a50f1490632c10ce85d9429a44510e13876aa
Srinivas <srinivaschari.y@gmail.com>
2018-09-28 15:01:31 +0530

    New Changees

--- ShootingGame/Assets/Srinivas/Scripts/RaycastGun.cs
+++ ShootingGame/Assets/Srinivas/Scripts/RaycastGun.cs
@@ -25,15 +25,15 @@ public class RaycastGun : MonoBehaviour {
 				for (int r = 0; r < 2; r++)
 				{
 				
-				GameObject temp1 = (GameObject)Instantiate(enemies1, new Vector3(-18 + 1.5f * r, -0.34f , 7.5f - 5f * t),	Quaternion.identity);
-
-				GameObject temp2 = (GameObject)Instantiate(enemies2, new Vector3(-14 + 1.5f * r, -1.30f , 7.5f - 5f * t),	Quaternion.identity);
-
-				GameObject temp3 = (GameObject)Instantiate(enemies3, new Vector3(-10 + 1.5f * r, -0.34f , 7.5f - 5f * t), Quaternion.identity);
-
-				GameObject temp4 = (GameObject)Instantiate(enemies4, new Vector3(-6 + 1.5f * r, -0.34f , 7.5f - 5f * t), Quaternion.identity);
-
-				GameObject temp5 = (GameObject)Instantiate(enemies5, new Vector3(-0 + 1.5f * r, -0.34f , 7.5f - 5f * t), Quaternion.identity);
+//				GameObject temp1 = (GameObject)Instantiate(enemies1, new Vector3(-18 + 1.5f * r, -0.34f , 7.5f - 5f * t),	Quaternion.identity);
+//
+//				GameObject temp2 = (GameObject)Instantiate(enemies2, new Vector3(-14 + 1.5f * r, -1.30f , 7.5f - 5f * t),	Quaternion.identity);
+//
+//				GameObject temp3 = (GameObject)Instantiate(enemies3, new Vector3(-10 + 1.5f * r, -0.34f , 7.5f - 5f * t), Quaternion.identity);
+//
+//				GameObject temp4 = (GameObject)Instantiate(enemies4, new Vector3(-6 + 1.5f * r, -0.34f , 7.5f - 5f * t), Quaternion.identity);
+//
+//				GameObject temp5 = (GameObject)Instantiate(enemies5, new Vector3(-0 + 1.5f * r, -0.34f , 7.5f - 5f * t), Quaternion.identity);
 
 				}
 			}
