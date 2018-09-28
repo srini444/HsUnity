65368809be4a116ec209e1977c979b60efdbb12e
Srinivas <srinivaschari.y@gmail.com>
2018-09-28 11:34:33 +0530

    Cam Rotation

--- ShootingGame/Assets/Srinivas/Scripts/CameraRotation.cs
+++ ShootingGame/Assets/Srinivas/Scripts/CameraRotation.cs
@@ -2,27 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraRotation : MonoBehaviour {
+public class CameraRotation : MonoBehaviour 
+{
 
-	public float speedH = 2.0f;
-	public float speedV = 2.0f;
+	public float speedH = 10.0f;
+	public float speedV = 10.0f;
 
 	private float yaw = 0.0f;
 	private float pitch = 0.0f;
-	void Start () 
+
+	void Update()
 	{
-		
+
+		if (Input.GetMouseButton (2)) {
+			yaw += speedH * Input.GetAxis ("Mouse X");
+			pitch -= speedV * Input.GetAxis("Mouse Y");
+
+			transform.eulerAngles = new Vector3 (pitch, yaw, 0.0f);
+		}
 	}
-	
 
-	void Update () 
-	{
-		
-		yaw += speedH * Input.GetAxis("Mouse X");
-		pitch -= speedV * Input.GetAxis("Mouse Y");
-
-		transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
-	
-
-	}
 }
