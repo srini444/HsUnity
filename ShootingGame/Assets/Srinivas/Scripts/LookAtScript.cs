9d131d4497449465ab1d2f9c1b3a2b705ad59da0
Srinivas <srinivaschari.y@gmail.com>
2018-10-01 13:15:01 +0530

    Add

---
+++ ShootingGame/Assets/Srinivas/Scripts/LookAtScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtScript : MonoBehaviour {
+
+
+	public GameObject target;
+	void Update () 
+	{
+		transform.LookAt (target.transform, Vector3.up);
+		transform.position = Vector3.Lerp (transform.position, target.transform.position, 0.001f);
+	}
+}
--- ShootingGame/Assets/Srinivas/Scripts/RaycastGun.cs
+++ ShootingGame/Assets/Srinivas/Scripts/RaycastGun.cs
@@ -10,34 +10,34 @@ public class RaycastGun : MonoBehaviour {
 	private float yaw = 0.0f;
 	private float pitch = 0.0f;
 
-	public GameObject ball;
-	public GameObject enemies1;
-	public GameObject enemies2;
-	public GameObject enemies3;
-	public GameObject enemies4;
+//	public GameObject ball;
+//	public GameObject enemies1;
+//	public GameObject enemies2;
+//	public GameObject enemies3;
+//	public GameObject enemies4;
 
 
 	void Start ()
 	{
 		
-			for (int t = 0; t < 2; t++)
-			{
-				for (int r = 0; r < 2; r++)
-				{
-				
-
-				//GameObject temp1 = (GameObject)Instantiate(enemies1, new Vector3(-6 + 1.5f * r, -0.47f , 7.5f - 5f * t), Quaternion.identity);
-
-				//GameObject temp2 = (GameObject)Instantiate(enemies2, new Vector3(-14 + 1.5f * r, -0.47f , 7.5f - 5f * t),	Quaternion.identity);
-
-				//GameObject temp3 = (GameObject)Instantiate(enemies3, new Vector3(-10 + 1.5f * r, -0.29f , 7.5f - 5f * t), Quaternion.identity);
-
-				//GameObject temp4 = (GameObject)Instantiate(enemies4, new Vector3(-5 + 1.5f * r, -0.29f , 7.5f - 5f * t), Quaternion.identity);
-
-
-				
-				}
-			}
+//			for (int t = 0; t < 2; t++)
+//			{
+//				for (int r = 0; r < 2; r++)
+//				{
+//				
+//
+//				//GameObject temp1 = (GameObject)Instantiate(enemies1, new Vector3(-6 + 1.5f * r, -0.47f , 7.5f - 5f * t), Quaternion.identity);
+//
+//				//GameObject temp2 = (GameObject)Instantiate(enemies2, new Vector3(-14 + 1.5f * r, -0.47f , 7.5f - 5f * t),	Quaternion.identity);
+//
+//				//GameObject temp3 = (GameObject)Instantiate(enemies3, new Vector3(-10 + 1.5f * r, -0.29f , 7.5f - 5f * t), Quaternion.identity);
+//
+//				//GameObject temp4 = (GameObject)Instantiate(enemies4, new Vector3(-5 + 1.5f * r, -0.29f , 7.5f - 5f * t), Quaternion.identity);
+//
+//
+//				
+//				}
+//			}
 
 
 	}
