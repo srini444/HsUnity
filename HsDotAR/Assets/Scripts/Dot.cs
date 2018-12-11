108cd6a7a47ba526f157ed39d0833dc2aae8da5d
Srinivas <srinivaschari.y@gmail.com>
2018-12-11 17:22:47 +0530

    AddAR

--- HsDotAR/Assets/Scripts/Dot.cs
+++ HsDotAR/Assets/Scripts/Dot.cs
@@ -18,7 +18,7 @@ public class Dot : MonoBehaviour
         CreateDot();
     }
 	
-	void Update ()
+    void Update ()
     {
         if (Input.GetMouseButtonDown(0))
         {
@@ -26,40 +26,40 @@ public class Dot : MonoBehaviour
         }
 
         if (Input.GetMouseButtonUp(0))
-            {
-                mouseEnd = Input.mousePosition;
-                Vector3 targetDir = target.position - transform.position;
-                Vector3 forward = transform.forward;
-                float angle = Vector3.SignedAngle(targetDir, forward, Vector3.up);
+        {
+            mouseEnd = Input.mousePosition;
+            Vector3 targetDir = target.position - transform.position;
+            Vector3 forward = transform.forward;
+            float angle = Vector3.SignedAngle(targetDir, forward, Vector3.up);
 
             if (angle < -10.0F)
-                {
-                    dotInsstance.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(Random.Range(-10.0f, 0f), Random.Range(-10.0f, 10.0f), Random.Range(50.0f, 10.0f)) * dotForce * Time.deltaTime, ForceMode.Impulse);
-                    print("turn left");
-                    Invoke("CreateDot", 3f);
-                }
+            {
+                dotInsstance.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-10.0f, 10.0f), Random.Range(50.0f, 10.0f)) * dotForce * Time.deltaTime, ForceMode.Impulse);
+                print("turn left");
+                Invoke("CreateDot", 3f);
+            }
                
-        else if (angle > 10.0F)
-                {
-                    dotInsstance.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(Random.Range(0.0f, 10.0f), Random.Range(-10.0f, 10.0f), Random.Range(5.0f, 10.0f)) * dotForce * Time.deltaTime, ForceMode.Impulse);
-                    print("turn right");
-                    Invoke("CreateDot", 3f);
+            else if (angle > 10.0F)
+            {
+                dotInsstance.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-10.0f, 10.0f), Random.Range(50.0f, 10.0f)) * dotForce * Time.deltaTime, ForceMode.Impulse);
+                print("turn right");
+                Invoke("CreateDot", 3f);
             }
             
-        else
-                {
-                    dotInsstance.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-10.0f, 10.0f), Random.Range(50.0f, 10.0f)) * dotForce * Time.deltaTime, ForceMode.Impulse);
-                    print("forward");
-                    Invoke("CreateDot", 4f);
-                }
-     
+            else
+            {
+                dotInsstance.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-10.0f, 10.0f), Random.Range(50.0f, 10.0f)) * dotForce * Time.deltaTime, ForceMode.Impulse);
+                print("forward");
+                Invoke("CreateDot", 3f);
             }
+     
+        }
 
-      	}
+    }
 
     void CreateDot()
     {
-      dotInsstance = Instantiate(dotPrefab, dotPrefab.transform.position, dotPrefab.transform.rotation) as GameObject;
+        dotInsstance = Instantiate(dotPrefab, dotPrefab.transform.position, dotPrefab.transform.rotation) as GameObject;
     }
 
 }
