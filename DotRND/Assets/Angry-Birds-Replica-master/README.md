6f19ac19f501bd8b2eda2d5a8c15951d657da18c
Srinivas <srinivaschari.y@gmail.com>
2018-12-14 17:49:08 +0530

    dONE cHANGES

--- DotRND/Assets/Angry-Birds-Replica-master/README.md
+++
@@ -1,8 +0,0 @@
-# Angry Birds Replica
-This is the source code for a Angry Birds Replica created during a Twitch livestream.
-
-Everything is made using Unity.
-
-Check out my [YouTube Channel](http://youtube.com/brackeys) for more tutorials. The livestream will be uploaded there as well.
-
-Everything is free to use also commercially (public domain).
---
+++ DotRND/Assets/Srinivas/SwipeDetector.cs
@@ -0,0 +1,128 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwipeDetector : MonoBehaviour
+{
+    private Vector3 fingerDown;
+    private Vector3 fingerUp;
+    public bool detectSwipeOnlyAfterRelease = false;
+
+    public float SWIPE_THRESHOLD = 20f;
+    Rigidbody rb;
+    public float speed = 50f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        foreach (Touch touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Began)
+            {
+                fingerUp = touch.position;
+                fingerDown = touch.position;
+            }
+
+            //Detects Swipe while finger is still moving
+            if (touch.phase == TouchPhase.Moved)
+            {
+                if (!detectSwipeOnlyAfterRelease)
+                {
+                    fingerDown = touch.position;
+                    checkSwipe();
+                }
+            }
+
+            //Detects swipe after finger is released
+            if (touch.phase == TouchPhase.Ended)
+            {
+                fingerDown = touch.position;
+                checkSwipe();
+            }
+        }
+    }
+
+    void checkSwipe()
+    {
+        //Check if Vertical swipe
+        if (verticalMove() > SWIPE_THRESHOLD && verticalMove() > horizontalValMove())
+        {
+            //Debug.Log("Vertical");
+            if (fingerDown.y - fingerUp.y > 0)//up swipe
+            {
+                OnSwipeUp();
+            }
+            else if (fingerDown.y - fingerUp.y < 0)//Down swipe
+            {
+                OnSwipeDown();
+            }
+            fingerUp = fingerDown;
+        }
+
+        //Check if Horizontal swipe
+        else if (horizontalValMove() > SWIPE_THRESHOLD && horizontalValMove() > verticalMove())
+        {
+            //Debug.Log("Horizontal");
+            if (fingerDown.x - fingerUp.x > 0)//Right swipe
+            {
+                OnSwipeRight();
+            }
+            else if (fingerDown.x - fingerUp.x < 0)//Left swipe
+            {
+                OnSwipeLeft();
+            }
+            fingerUp = fingerDown;
+        }
+
+        //No Movement at-all
+        else
+        {
+            //Debug.Log("No Swipe!");
+        }
+    }
+
+    float verticalMove()
+    {
+        return Mathf.Abs(fingerDown.y - fingerUp.y);
+    }
+
+    float horizontalValMove()
+    {
+        return Mathf.Abs(fingerDown.x - fingerUp.x);
+    }
+
+    //////////////////////////////////CALLBACK FUNCTIONS/////////////////////////////
+    void OnSwipeUp()
+    {
+        Debug.Log("Swipe UP");
+        rb.AddForce(Random.Range(-1.0f, 1.0f), 5f, 15f, ForceMode.Impulse);
+        //  rb.AddForce(0f,5f, 15f ,ForceMode.VelocityChange);
+        //  rb.AddForce(0f, 5f, 15f, ForceMode.Acceleration);
+        //  rb.AddForce(0f, 5f, 15f, ForceMode.Force);
+
+
+    }
+
+    void OnSwipeDown()
+    {
+        Debug.Log("Swipe Down");
+    }
+
+    void OnSwipeLeft()
+    {
+        Debug.Log("Swipe Left");
+        rb.AddForce(Random.Range(-0.5f, -4f), 5f, 10f, ForceMode.Impulse);
+    }
+
+    void OnSwipeRight()
+    {
+        Debug.Log("Swipe Right");
+        rb.AddForce(Random.Range(0.5f, 4f), 5f, 10f, ForceMode.Impulse);
+
+    }
+}
