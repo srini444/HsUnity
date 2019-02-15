2e3f0dee388d7f51249a86f588d9079d2243c6f1
Srinivas <srinivaschari.y@gmail.com>
2019-02-15 17:38:07 +0530

    Parking Game

---
+++ CarGame3D/Assets/CamScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamScript : MonoBehaviour {
+
+    public Camera[] cams;
+	
+    public void camOne()
+    {
+        cams[0].enabled = true;
+        cams[1].enabled = false;
+    }
+
+    public void camMain()
+    {
+        cams[0].enabled = false;
+        cams[1].enabled = true;
+    }
+
+
+    void Update ()
+    {
+		
+	}
+}
